7fe0a8ce169be5798b8b5c25d8fd791a76a4a56b
 TempVolodymyr <nativeassasin1988@gmail.com>
2021-01-18 12:07:11 +0200

    Initial Commit

---
+++ FastTranslator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FastTranslator
+//
+//  Created by User1 on 18.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
